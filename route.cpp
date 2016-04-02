38d6cde6cb16600609fd297867dda462db7a46b1
liuzhuoling <liuzhuoling2011@hotmail.com>
2016-04-02 21:06:14 +0800

    建立好了map临接表

--- route.cpp
+++ route.cpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
@@ -14,8 +15,10 @@ struct Edge{
     int weight;
 };
 
-vector< vector<Edge> > Graph;
+vector< vector<Edge> > Graph;   //vector临接表
 
+map< int, vector<Edge> > mGraph;    //map临接表
+map< int, vector<Edge> >::iterator it;
 
 bool Valid() {
 
@@ -24,21 +27,34 @@ bool Valid() {
 //你要完成的功能总入口
 void search_route(char *topo[5000], int edge_num, char *demand)
 {
-    unsigned short result[] = {2, 6, 3};//示例中的一个解
-
-    for (int i = 0; i < 3; i++)
-        record_result(result[i]);
 
     int index, start, end, weight;
 
-    Graph.resize(edge_num+1);
+//    unsigned short result[] = {2, 6, 3};//示例中的一个解
+//
+//    for (int i = 0; i < 3; i++)
+//        record_result(result[i]);
+
+//    Graph.resize(edge_num+1);
 
     for(int i = 0; i < edge_num; i++) {
         sscanf(topo[i], "%d,%d,%d,%d", &index, &start, &end, &weight);
 
-        Graph[start].push_back( Edge(index, end, weight) );
+        it = mGraph.find(start);
+        if (mGraph.find(start) != mGraph.end()){
+            it->second.push_back( Edge(index, end, weight) );
+        }else{
+            vector<Edge> neighbor;
+            neighbor.push_back( Edge(index, end, weight) );
+            mGraph.insert(make_pair(start, neighbor));
+        }
+
+//        Graph[start].push_back( Edge(index, end, weight) );
 
     }
+
+    vector<Edge> tempV = mGraph.find(3)->second;
+    
 }
 
 

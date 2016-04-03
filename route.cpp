dcb4a5187204cd5b9f42503cba1d0aac2b787360
liuzhuoling <liuzhuoling2011@hotmail.com>
2016-04-03 13:21:59 +0800

    目前已经fix DFS bugs, 找到所有符合的paths

--- route.cpp
+++ route.cpp
@@ -29,8 +29,24 @@ vector<Edge> paths;
 vector< vector<Edge> > totalPaths;
 
 int startNode;
-int currentNode;
 int endNode;
+
+bool isContain( vector<Edge> path, vector<int> V){  //Derek
+    bool flag;
+    for(vector<int>::iterator i = V.begin(); i < V.end(); i++) {
+        flag = false;
+        for(vector<Edge>::iterator j = path.begin(); j < path.end(); j++) {
+            if (j->end == *i) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) {
+            return false;
+        }
+    }
+    return true;
+}
 
 bool Valid(int current) {
     if( isVisited.find(current)->second == false)
@@ -41,30 +57,28 @@ bool Valid(int current) {
 
 void DFS(Edge thisEdge) {
 
-    paths.push_back(thisEdge);
-    isVisited.find(thisEdge.end)->second = true;
+    if(mGraph.find(thisEdge.end) == mGraph.end() && thisEdge.end != endNode) return;   //说明它不是终点也没有指出去的边了
 
     if(thisEdge.end == endNode) {
         vector<Edge> path(paths);
-        if (paths.size() ==1) paths.pop_back();
-        if (isVisited.find(endNode)->second == true) isVisited.find(endNode)->second = false;
         totalPaths.push_back(path);
         return;
     }
 
-    if(mGraph.find(thisEdge.end) == mGraph.end()) return;
-
     vector<Edge> nextEdge(mGraph.find(thisEdge.end)->second);
 
     for (int i = 0; i < nextEdge.size(); i++) {
-        currentNode = nextEdge[i].end;
+        int currentNode = nextEdge[i].start;
         if (Valid(currentNode)) {
-//            isVisited.find(currentNode)->second = true;
+            isVisited.find(currentNode)->second = true;
+            paths.push_back(nextEdge[i]);
             DFS(nextEdge[i]);
             paths.pop_back();
             isVisited.find(currentNode)->second = false;
         }
     }
+
+
 }
 
 //你要完成的功能总入口
@@ -115,10 +129,17 @@ void search_route(char *topo[5000], int edge_num, char *demand)
     isVisited.find(startNode)->second = true;
     vector<Edge> startV = mGraph.find(startNode)->second;
     for (int i = 0; i < startV.size(); i++) {
+        paths.push_back(startV[i]);
         DFS(startV[i]);
+        paths.pop_back();
     }
 
-    cout<<"hello";
+    for( int i=0; i < totalPaths.size(); i++){
+        if( isContain(totalPaths[i],V1)){
+            //Todo
+            cout << "在totalPaths中序号为 "<< i << " 的序列符合要求" << endl;
+        }
+    }
 }
 
 
